4d9f4285509a15819c6b82da48ae8e4091e2cbc5
pasha450 <pasha.hipl@gmail.com>
2025-01-11 12:57:50 +0530

    pass message into login

--- controllers/authApiControllers.js
+++ controllers/authApiControllers.js
@@ -20,7 +20,7 @@ async function register(req, res ) {
         const{name , email , password} =  req.body;
         const user = await User.findOne({email :email});
         if(user){
-            return res.status(401).json({status: false , error:"Email already in use!"});
+            return res.status(409).json({status: false , error:"Email already in use!"});
         }
         await User.create(req.body);
         res.status(200).json({status: true , message:"User Created Successfully"})
@@ -45,9 +45,8 @@ async function login(req,res){
         }
         const token = jwt.sign({ userId: user._id }, process.env.SESSION_SECRET, {
             expiresIn: '2h',
-        
         });
-        res.status(200).json({status: true, user});
+        res.status(200).json({status: true, user ,message:"User Login Successfully"});
     } catch (error) {
         console.log(error);
         res.status(500).json({ error: 'Login failed' });
